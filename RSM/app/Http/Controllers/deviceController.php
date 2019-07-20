0170184dd39d4566fbdd1955b29794206934a1df
Moha-Mohajer <mohajeranii@hotmail.com>
2019-07-20 20:49:32 +0300

    divice controler finish as sample

--- RSM/app/Http/Controllers/deviceController.php
+++ RSM/app/Http/Controllers/deviceController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+// Use Eloquent
+use App\Device;
+// Use SQL qurry by bringing DB Librely
+use  DB;
 
 class deviceController extends Controller
 {
@@ -13,7 +17,26 @@ class deviceController extends Controller
      */
     public function index()
     {
-        //
+
+        // // // Use SQL qurry
+        // // $devices = DB::select('SELECT * FROM devices');
+        // // Order by 
+        // $devices = DB::select('SELECT * FROM devices ORDER BY title DESC');
+
+
+        // // // // // Use Eloquent
+        // // // // $devices = Device::all();
+        // // // // Order by 
+        // // // $devices = Device::orderBy('title', 'desc')->get();
+        // // // specified
+        // // return Device::where('title', 'Device Two')->get();
+        // // Just take 1 
+        // $devices = Device::orderBy('title', 'desc')->take(1)->get();
+        // paginate (number the page)
+        $devices = Device::orderBy('created_at', 'desc')->paginate(10);
+
+        // Load the view
+        return view ('devices.index')->with('devices',$devices);
     }
 
     /**
@@ -23,7 +46,8 @@ class deviceController extends Controller
      */
     public function create()
     {
-        //
+        //Load up the view
+        return view('devices.create');
     }
 
     /**
@@ -34,7 +58,21 @@ class deviceController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // Validation of input data
+        $this->validate($request,[
+            'title' =>'required',
+            'body' =>'required'
+        ]);
+
+        // Create Device
+        $Device = new Device;
+        $Device->title = $request->input('title');
+        $Device->body = $request->input('body');
+        $Device->save();
+
+        // redirect with success message
+        return redirect('/devices')->with('success', 'Device Created');
+
     }
 
     /**
@@ -45,7 +83,12 @@ class deviceController extends Controller
      */
     public function show($id)
     {
-        //
+        //fatche it with database "Eloquent"
+        // return Device::find($id);
+
+        // return the view
+        $Device = Device::find($id);
+        return view ('devices.show')->with('Device', $Device);
     }
 
     /**
@@ -56,7 +99,9 @@ class deviceController extends Controller
      */
     public function edit($id)
     {
-        //
+       // return the view
+       $Device = Device::find($id);
+       return view ('devices.edit')->with('Device', $Device);
     }
 
     /**
@@ -68,7 +113,20 @@ class deviceController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+
+        // Update Device
+        $Device = Device::find($id);
+        $Device->type = $request->input('type');
+        $Device->brand = $request->input('brand');
+        $Device->model = $request->input('model');
+        $Device->sn = $request->input('sn');
+        $Device->uid = $request->input('uid');
+        $Device->date = $request->input('date');
+        $Device->note = $request->input('note');
+        $Device->save();
+
+        // redirect with success message
+        return redirect('/devices')->with('success', 'Device Updated');
     }
 
     /**
@@ -79,6 +137,11 @@ class deviceController extends Controller
      */
     public function destroy($id)
     {
-        //
+        // Update delete
+        $Device = Device::find($id);
+        $Device->delete();
+
+        // redirect with success message
+        return redirect('/devices')->with('success', 'Device Updated');
     }
 }
